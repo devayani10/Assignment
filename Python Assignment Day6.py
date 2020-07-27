879cdf85fcc863f17424688a434702ad9fe5cd14
devayani10 <Devayani10022000@gmail.com>
2020-07-27 23:16:31 +0530

    Add files via upload

---
+++ Python Assignment Day6.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+list1 = [1,2,3,4,5,6,7,8]
+list2 = ["a","b","c","d","e"]
+print({list1[i]: list2[i]for i in range(0,len(list2))})
+
+
+# In[ ]:
+
+
+
+
